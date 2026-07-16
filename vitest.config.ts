1dd1cc36befa91b38e71d0fa506062a7a1cfc53b
3m1n3nc3 <mygames.ng@gmail.com>
2026-07-16 02:17:58 +0100

    remove unplugin-swc and use oxc config to support experimental decorator testing.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,36 +1,14 @@
-import { URL, fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
-import swc from 'unplugin-swc'
-
-const resolvePath = (path: string) => fileURLToPath(new URL(path, import.meta.url))
-
 
 export default defineConfig({
-    plugins: [
-        swc.vite({
-            jsc: {
-                parser: {
-                    syntax: 'typescript',
-                    decorators: true,
-                },
-                transform: {
-                    decoratorMetadata: true,
-                    legacyDecorator: true,
-                },
-                target: 'es2021',
-            },
-        }) as never,
-    ],
+    oxc: {
+        decorator: {
+            legacy: true,
+            emitDecoratorMetadata: true
+        }
+    },
     resolve: {
-        alias: {
-            '@': resolvePath('./src'),
-            'src': resolvePath('./src'),
-            '@app': resolvePath('./src/app'),
-            '@core': resolvePath('./src/core'),
-            '@controllers': resolvePath('./src/app/http/controllers'),
-            '@models': resolvePath('./src/app/models'),
-        },
+        tsconfigPaths: true
     },
     test: {
         environment: 'node',
